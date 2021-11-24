38991df06896727192369bf81cd195de0123f4c6
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-11-24 19:16:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ mongoose.connect("mongodb://localhost:27017/authentication")
     console.log(err);
 })
 app.listen(3000);
+
+// schema for user 
+const userSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    username:{type:String,required:true},
+    password:{type:String,required:true}
+},
+{
+    timestamps:true
+});
+
+const userModel = new mongoose.model("users",userSchema); 
+
+// to register a user
+
+app.post("/register",(req,res)=>{
+    
+    let user = req.body;
+    let userOBJ = new userModel(user);
+
+    userOBJ.save()
+    .then(()=>{
+        res.send({message:"User is Registered"})
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.send({message:"Problem in creating the user"}) 
+    })
+
+})
