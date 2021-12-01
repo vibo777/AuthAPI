77e802da68bc8369ea68e48da846e5975035e72d
vibo777 <54715728+vibo777@users.noreply.github.com>
2021-12-01 15:39:04 +0530

    Finished with file uploading

--- fileupload.js
+++ fileupload.js
@@ -1,6 +1,7 @@
 // import the express & formidable library
 const express = require('express');
 const formidable = require('formidable');
+const fs = require('fs');
 
 // create object
 const app = express();
@@ -14,16 +15,22 @@ app.post("/create",(req,res)=>{
     //for handling new request .i.e file/data we need to create a formidale object 
     // here we are sure we will reciving a file
     const form = new formidable.IncomingForm();
+
+    console.log(req.headers);
     
     // we simply parse incoming request by using parse() method by passing req 
     form.parse(req,(err,body,files)=>{
 
+        let extension = files.picture.originalFilename.split(".")[1];
+
         console.log(body);
-        console.log(files);
-        res.send({message:"Everything works"});
-
+        fs.rename(files.picture.filepath,"./uploads/"+files.picture.newFilename+"."+extension,(err)=>{
+            if(err===null){
+                res.send({message:"Everything works"});
+            }
+        }); 
+        
     })
-
 })
 
 app.listen(3000);
